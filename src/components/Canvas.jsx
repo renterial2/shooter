53b5746fdf375e5f011ae8c582af73e1c41312f6
RenteriaL <renterial@thesharepointprofessional.com>
2019-03-03 19:26:11 -0600

    Added ground, sky, cannon, cannonball, and aiming functionality

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Sky from './Sky';
+import Ground from './Ground';
+import CannonBase from './CannonBase';
+import CannonPipe from './CannonPipe';
+import CannonBall from './CannonBall'
+import CurrentScore from './CurrentScore';
+
+const Canvas = (props) => {
+  const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+  return (
+    <svg
+      id="aliens-go-home-canvas"
+      preserveAspectRatio="xMaxYMax none"
+      onMouseMove={props.trackMouse}
+      viewBox={viewBox}
+    >
+    
+    <defs>
+  <filter id="shadow">
+    <feDropShadow dx="1" dy="1" stdDeviation="2" />
+  </filter>
+</defs>
+<defs>
+  <filter id="shadow">
+    <feDropShadow dx="1" dy="1" stdDeviation="2" />
+  </filter>
+</defs>ß
+      <Sky />
+      <Ground />
+      <CannonPipe rotation={props.angle} />
+      <CannonBase />
+      <CannonBall position={{x: 0, y: -100}}/>
+      <CurrentScore score={15} />
+    </svg>
+  );
+};
+
+Canvas.propTypes = {
+  angle: PropTypes.number.isRequired,
+  trackMouse: PropTypes.func.isRequired,
+};
+
+export default Canvas;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
+import Game from './containers/Game';
+import reducer from './reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import url('https://fonts.googleapis.com/css?family=Joti+One');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* other rules ... */
+
+// import url('https://fonts.googleapis.com/css?family=Joti+One');
+
+/* other rules ... */
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+    reducer, /* preloadedState, */
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Game />
+    </Provider>,
+    document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
