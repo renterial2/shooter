4fe366de104da8dd865caa4364858370366f0fd4
RenteriaL <renterial@thesharepointprofessional.com>
2019-03-04 19:44:58 -0600

    added updating lives and score

--- src/reducers/moveObjects.js
+++ src/reducers/moveObjects.js
@@ -20,6 +20,19 @@ function moveObjects(state, action) {
     (now - object.createdAt) < 4000
   ))
 
+  const lostLife = state.gameState.flyingObjects.length > flyingObjects.length
+  let lives = state.gameState.lives
+  if (lostLife) {
+    lives--
+  }
+
+  const started = lives > 0
+  if (!started) {
+    flyingObjects = []
+    cannonBalls = []
+    lives = 3
+  }
+
   const { x, y } = mousePosition
   const angle = calculateAngle(0, 0, x, y)
 
@@ -30,12 +43,17 @@ function moveObjects(state, action) {
   cannonBalls = cannonBalls.filter(cannonBall => (cannonBallsDestroyed.indexOf(cannonBall.id)))
   flyingObjects = flyingObjects.filter(flyingDisc => (flyingDiscsDestroyed.indexOf(flyingDisc.id)))
 
+  const kills = state.gameState.kills + flyingDiscsDestroyed.length
+
   return {
     ...newState,
     gameState: {
       ...newState.gameState,
       flyingObjects,
-      cannonBalls,
+      cannonBalls: [...cannonBalls],
+      lives,
+      started,
+      kills,
     },
     angle,
   }
